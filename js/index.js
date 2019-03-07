00b080d2c67415eb6c69a17db14020737ff5289b
jsriyun <jsriyun@gmail.co.kr>
2019-03-07 14:13:25 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -1,99 +1,77 @@
-//마이메뉴  start
-	// $(function () {
-	// 	$(".mymenu_select").click(
-	// 		function () {
-	// 			$(this).addClass("active");
-	// 		});
-	// });
-
-  $(function () {
-    $(".mymenu_select_btn").focus(function () {
-        $(".mymenu_select").addClass("active");
-    });
-    $(".mymenu_close_focus").blur(function () {
-        $(".mymenu_select").removeClass("active");
-    });
-    $(".mymenu_close_focus_sub").focus(function () {
-        $(".mymenu_select").removeClass("active");
-    });
-    $(window).click(function () {
-        $(".mymenu_select").removeClass("active");
-    });
+$(function() {
+  $(".mymenu_select_btn").focus(function () {
+      $(".mymenu_select").addClass("active");
   });
+  $(".mymenu_close_focus").blur(function () {
+      $(".mymenu_select").removeClass("active");
+  });
+  $(".mymenu_close_focus_sub").focus(function () {
+      $(".mymenu_select").removeClass("active");
+  });
+  $(window).click(function () {
+      $(".mymenu_select").removeClass("active");
+  });
+});
 
-	$(function () {
-		$(".mymenu_close").click(
-			function(){
-			  $(".mymenu_select").removeClass("active");
-		});
-    $('.mymenu_close').on("click",function(){
-      $('.mymenu_select').focus();
-    });
+$(function () {
+	$(".mymenu_close").click(
+		function(){
+		  $(".mymenu_select").removeClass("active");
 	});
+  $('.mymenu_close').on("click",function(){
+    $('.mymenu_select').focus();
+  });
+});
 
 
 //마이메뉴 end
 
 // 팝업 start
-;(function($) {
-    $(function() {
-      for (var i = 1; i <= 10; i++){
+;(function($){
+  $(function(){
+    for (var i = 1; i <= 10; i++){
 
-				$("#myBtn"+i).data({pop:$("#myModal"+i)});
-				$('#myBtn'+ i).on("click",function(e){
-					e.preventDefault();
-					$(this).data("pop").bPopup({
-						// follow: [false, false]
-					});
-          $(".default_cursor").focus();
+			$("#myBtn"+i).data({pop:$("#myModal"+i)});
+			$("#myBtn"+i).on("click",function(e){
+				e.preventDefault();
+				$(this).data("pop").bPopup({
+					// follow: [false, false]
 				});
-				$('.b-close1').on("click",function(){
-					$('.popupfocus1').focus();
+        $(".default_cursor").focus();
+			});
+
+      //웹접근성:팝업클로즈 버튼 누르면, 포커스가 그전에 눌렀던 팝업 버튼으로 감
+      for (var d = 1; d <= 10; d++){
+        $('.b-close'+d).data({pop_focus:$('.popupfocus'+d)});
+				$('.b-close'+d).on("click",function(){
+					$(this).data("pop_focus").focus();
 				});
-				$('.b-close2').on("click",function(){
-					$('.popupfocus2').focus();
-				});
-        $('.b-close3').on("click",function(){
-					$('.popupfocus3').focus();
-				});
-				$('.b-close4').on("click",function(){
-					$('.popupfocus4').focus();
-				});
-        $('.b-close5').on("click",function(){
-					$('.popupfocus5').focus();
-				});
-				$('.b-close6').on("click",function(){
-					$('.popupfocus6').focus();
-				});
-        $('.b-close7').on("click",function(){
-					$('.popupfocus7').focus();
-				});
-				$('.b-close8').on("click",function(){
-					$('.popupfocus8').focus();
-				});
-        $('.b-close9').on("click",function(){
-					$('.popupfocus9').focus();
-				});
-				$('.close').on("click",function(){
-					$('.mymenu_select').focus();
-				});
-			};
-    });
- })(jQuery);
+      }
+      // $('.b-close1').on("click",function(){
+      //   $('.popupfocus1').focus();
+      // });
+      $('.close').on("click",function(){
+        $('.mymenu_select').focus();
+      });
+
+
+		};
+  });
+})(jQuery);
 // 팝업 end
 
-	$(".branch").click(function(){
-    $(this).children().toggleClass("on");
-		$(this).next().slideToggle()
-	});
+$(".branch").click(function(){
+  $(this).children().toggleClass("on");
+	$(this).next().slideToggle()
+});
 
-	$(function(){
-		$().each(function(){
-			$(this).click(function(){
-				var name_var=$(this).attr();
-			});
+$(function(){
+	$().each(function(){
+		$(this).click(function(){
+			var name_var=$(this).attr();
 		});
 	});
+});
 
 /*모달팝업 start*/
 
@@ -130,28 +108,28 @@ uploadFile.on('change', function(){
 /*탭메뉴 추가 start*/
 $(function () {
 
-    $(".tab_content").hide();
-    $(".tab_content:first").show();
+  $(".tab_content").hide();
+  $(".tab_content:first").show();
 
-    $("ul.tabs li").click(function () {
-        $("ul.tabs li").removeClass("active").css("color", "#979797");
-        //$(this).addClass("active").css({"color": "darkred","font-weight": "bolder"});
-        $(this).addClass("active").css("color", "#4283df");
-        $(".tab_content").hide();
-        var activeTab = $(this).attr("rel");
-        $("#" + activeTab).fadeIn();
-    });
+  $("ul.tabs li").click(function () {
+    $("ul.tabs li").removeClass("active").css("color", "#979797");
+    //$(this).addClass("active").css({"color": "darkred","font-weight": "bolder"});
+    $(this).addClass("active").css("color", "#4283df");
+    $(".tab_content").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
+  });
 });
 /*탭메뉴 추가 end*/
 
 /*select 추가 start*/
 jQuery(document).ready(function(){
 
-    var select = $("select");
+  var select = $("select");
 
-    select.change(function(){
-        var select_name = $(this).children("option:selected").text();
-        $(this).siblings("label").text(select_name);
-    });
+  select.change(function(){
+    var select_name = $(this).children("option:selected").text();
+    $(this).siblings("label").text(select_name);
+  });
 });
 /*select 추가 end*/
